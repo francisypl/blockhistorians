9da67838bfcbf3e8dd5ee228b76a4c11a03eb582
Francis Yuen <fyuen@godaddy.com>
2018-02-18 11:18:51 -0700

    i know contract tests are failing

--- contracts/BlockHistorians.sol
+++ contracts/BlockHistorians.sol
@@ -3,29 +3,32 @@ pragma solidity ^0.4.18;
 import './SafeMath.sol';
 
 contract BlockHistorians {
-  using SafeMath for uint256;  
+  using SafeMath for uint;  
 
   struct Proposal {
 		address historian;
-    uint256 votes;
+    uint votes;
+    uint hashLocation;
     mapping(address => bool) voters;
 	}
 
   address _owner;
-  bytes32[] public entries;
-  mapping(bytes32 => Proposal) public proposals;
+  uint[] public entries;
+  string[] public hashes;
+  uint public hashCount;
+  Proposal[] public proposals;
   mapping(address => bool) public historians;
-  uint256 public numHistorians;
+  uint public numHistorians;
 
   function BlockHistorians() public {
     _owner = msg.sender;
   }
 
   function getVersion() public pure returns (string) {
-    return 'blockhistorians_v0.0.1';
+    return 'blockhistorians_v0.1.0';
   }
 
-  function getPassThreshold() public view returns (uint256) {
+  function getPassThreshold() public view returns (uint) {
     return numHistorians * 2 / 3;
   }
 
@@ -36,35 +39,78 @@ contract BlockHistorians {
     }
   }
 
-  function addProposal(bytes32 proposal) public {
-    proposals[proposal] = Proposal(msg.sender, 0);
+  function addProposal(string proposal) public {
+    // proposals[proposal] = Proposal(msg.sender, 0);
+    hashes.push(proposal);
+    proposals.push(Proposal(msg.sender, 0, hashCount));
+    hashCount = hashCount.add(1);
   }
 
-  function vote(bytes32 proposal, bool isUpvote) public {
-    assert(!proposals[proposal].voters[msg.sender]);
-    proposals[proposal].voters[msg.sender] = true;
-    uint256 threshold = getPassThreshold();
+  function compareStrings (string a, string b) private pure returns (bool){
+    return keccak256(a) == keccak256(b);
+  }
+
+  function vote(string proposal, bool isUpvote) public {
+    uint propLoc;
+
+    for (uint x = 0; x < proposals.length; x++) {
+      if (compareStrings(hashes[proposals[x].hashLocation], proposal)) {
+        propLoc = x;
+        break;
+      }
+    }
+
+    assert(!proposals[propLoc].voters[msg.sender]);
+    proposals[propLoc].voters[msg.sender] = true;
+    uint threshold = getPassThreshold();
     if (isUpvote) {
-      proposals[proposal].votes = proposals[proposal].votes.add(1);
-      if (proposals[proposal].votes > threshold) {
-        entries.push(proposal);
+      proposals[propLoc].votes = proposals[propLoc].votes.add(1);
+      if (proposals[propLoc].votes > threshold) {
+        entries.push(proposals[propLoc].hashLocation);
       }
     } else {
-      if (proposals[proposal].votes > 0) {
-        proposals[proposal].votes = proposals[proposal].votes.sub(1);
+      if (proposals[propLoc].votes > 0) {
+        proposals[propLoc].votes = proposals[propLoc].votes.sub(1);
       }
     }
   }
 
-  function getVotes(bytes32 proposal) public view returns (uint256) {
-    return proposals[proposal].votes;
+  function getVotes(string proposal) public view returns (uint) {
+    uint propLoc;
+    for (uint x = 0; x < proposals.length; x++) {
+      if (compareStrings(hashes[proposals[x].hashLocation], proposal)) {
+        propLoc = x;
+        break;
+      }
+    }
+    return proposals[propLoc].votes;
   }
 
-  function getProposalHistorian(bytes32 proposal) public view returns (address) {
-    return proposals[proposal].historian;
+  function getProposalHistorian(string proposal) public view returns (address) {
+    uint propLoc;
+    for (uint x = 0; x < proposals.length; x++) {
+      if (compareStrings(hashes[proposals[x].hashLocation], proposal)) {
+        propLoc = x;
+        break;
+      }
+    }
+    return proposals[propLoc].historian;
   }
 
-  function getEntries() public view returns (bytes32[]) {
+  function getEntries() public view returns (uint[]) {
     return entries;
+  }
+
+  function getHashAtIndex(uint index) public view returns (string) {
+    return hashes[index];
+  }
+
+  function getProposals() public view returns (uint[]) {
+    // string[] storage proposalHashes = new string[](proposals.length);
+    uint[] memory proposalHashes = new uint[](proposals.length);
+    for (uint x = 0; x < proposals.length; x++) {
+      proposalHashes[x] = proposals[x].hashLocation;
+    }
+    return proposalHashes;
   }
 }
--- src/BlockHistoriansContractWrapper.js
+++ src/BlockHistoriansContractWrapper.js
@@ -25,13 +25,16 @@ export default class BlockHistoriansContractWrapper {
     // below is mock function
     const entries = await this.instance.getEntries.call();
     console.log('entries =>', entries);
-    const promises = [];
-    for (let i = 0; i < entries.length; i++) {
-      const promise = ipfsFunctions.getContentWithHash(this.web3.toAscii(entries[i]));
-      promises.push(promise);
-    }
 
-    return await Promise.all(promises)
+    const indexPromises = entries.map(entry => {
+      return this.instance.getHashAtIndex(entry);
+    });
+    const hashes = await Promise.all(indexPromises);
+    const content = hashes.map(hash => {
+      return ipfsFunctions.getContentWithHash(hash);
+    });
+
+    return await Promise.all(content).map(single => JSON.parse(single));
   }
 
   async addHistorian() {
@@ -39,43 +42,61 @@ export default class BlockHistoriansContractWrapper {
   }
 
   async addProposal(hash) {
-    const hexHash = this.web3.fromAscii(hash, 32);
-    console.log('addProposal: hexHash =>', hexHash);
-    return await this.instance.addProposal(hexHash, { from: this.account });
+    console.log('addProposal: hexHash =>', hash);
+    return await this.instance.addProposal(hash, { from: this.account });
   }
 
   async vote(hash, isUpvote) {
-    const hexHash = this.web3.fromAscii(hash, 32);
-    console.log('vote: hexHash =>', hexHash, isUpvote);
-    return await this.instance.vote(hexHash, isUpvote, { from: this.account });
+    console.log('vote: hexHash =>', hash, isUpvote);
+    return await this.instance.vote(hash, isUpvote, { from: this.account });
   }
 
   async getProposals() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        return resolve([
-          {
-            date: 'date 1',
-            text: 'news 1 aosndf',
-            resource: ['https://i.imgur.com/vdKC9kr.jpg'],
-            hash: 'QmQFdyANXLmgxZPbGparNQ2evpygomjt2j1QMDU2SnSzcQ'
-          },
-          {
-            date: 'date 2',
-            text: 'news 2 lalala',
-            resource: ['https://i.imgur.com/ygZ5PGA.jpg'],
-            hash: ''
-          },
-          {
-            date: 'date 3',
-            text: 'news 3 foo bar',
-            resource: ['https://i.imgur.com/eqphLAA.jpg'],
-            hash: ''
-          }
-        ]);
-      }, 500);
+    const proposals = await this.instance.getProposals.call();
+    console.log('proposals =>', proposals);
+
+    const indexPromises = proposals.map(entry => {
+      return this.instance.getHashAtIndex(entry);
+    });
+    const hashes = await Promise.all(indexPromises);
+    const content = hashes.map(hash => {
+      return ipfsFunctions.getContentWithHash(hash);
+    });
+    const data = Promise.all(content);
+
+    return data.map((single, index) => {
+      const piece = JSON.parse(single);
+      piece.hash = hashes[index];
+      return piece;
     });
   }
+
+  // async getProposals() {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       return resolve([
+  //         {
+  //           date: 'date 1',
+  //           text: 'news 1 aosndf',
+  //           resource: ['https://i.imgur.com/vdKC9kr.jpg'],
+  //           hash: 'QmQFdyANXLmgxZPbGparNQ2evpygomjt2j1QMDU2SnSzcQ'
+  //         },
+  //         {
+  //           date: 'date 2',
+  //           text: 'news 2 lalala',
+  //           resource: ['https://i.imgur.com/ygZ5PGA.jpg'],
+  //           hash: ''
+  //         },
+  //         {
+  //           date: 'date 3',
+  //           text: 'news 3 foo bar',
+  //           resource: ['https://i.imgur.com/eqphLAA.jpg'],
+  //           hash: ''
+  //         }
+  //       ]);
+  //     }, 500);
+  //   });
+  // }
 }
 
 // async getEntries() {
