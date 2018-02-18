cccdc2d3f56cea1d55815c8a2e08f152d89f53bd
blockchainy <therulerguy@gmail.com>
2018-02-18 01:47:22 -0700

    fixed getEntries

--- src/BlockHistoriansContractWrapper.js
+++ src/BlockHistoriansContractWrapper.js
@@ -1,4 +1,5 @@
 import BlockHistoriansContract from '../build/contracts/BlockHistorians.json'
+import ipfsFunctions from '../src/utils/getIpfsHash';
 
 export default class BlockHistoriansContractWrapper {
   constructor(web3) {
@@ -17,42 +18,16 @@ export default class BlockHistoriansContractWrapper {
   }
 
   async getEntries() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        return resolve([
-          {
-            date: 'date 1',
-            text: 'news 1 aosndf',
-            resource: ['https://i.imgur.com/vdKC9kr.jpg']
-          }, 
-          {
-            date: 'date 2',
-            text: 'news 2 lalala',
-            resource: ['https://i.imgur.com/ygZ5PGA.jpg']
-          },
-          {
-            date: 'date 3',
-            text: 'news 3 foo bar',
-            resource: ['https://i.imgur.com/eqphLAA.jpg']
-          },
-          {
-            date: 'date 1',
-            text: 'news 1 aosndf',
-            resource: ['https://i.imgur.com/vdKC9kr.jpg']
-          }, 
-          {
-            date: 'date 2',
-            text: 'news 2 lalala',
-            resource: ['https://i.imgur.com/ygZ5PGA.jpg']
-          },
-          {
-            date: 'date 3',
-            text: 'news 3 foo bar',
-            resource: ['https://i.imgur.com/eqphLAA.jpg']
-          }
-        ]);
-      }, 500);
-    });
+    // below is mock function
+    const entries = await this.instance.getEntries().call();
+
+    const promises = [];
+    for (let i = 0; i < entries.length; i++) {
+      let promise = ipfsFunctions.getContentWithHash(this.web3.toAscii(entries[i]));
+      promises.push(promise);
+    }
+
+    return await Promise.all(promises)
   }
 
   async getProposals() {
@@ -63,7 +38,7 @@ export default class BlockHistoriansContractWrapper {
             date: 'date 1',
             text: 'news 1 aosndf',
             resource: ['https://i.imgur.com/vdKC9kr.jpg']
-          }, 
+          },
           {
             date: 'date 2',
             text: 'news 2 lalala',
@@ -78,7 +53,7 @@ export default class BlockHistoriansContractWrapper {
             date: 'date 1',
             text: 'news 1 aosndf',
             resource: ['https://i.imgur.com/vdKC9kr.jpg']
-          }, 
+          },
           {
             date: 'date 2',
             text: 'news 2 lalala',
@@ -94,3 +69,45 @@ export default class BlockHistoriansContractWrapper {
     });
   }
 }
+
+// async getEntries() {
+//   const entries = this.instance.getEntries().call();
+//   // for each entry hash get the data from ipfs
+//   // package data to look like
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       return resolve([
+//         {
+//           date: 'date 1',
+//           text: 'news 1 aosndf',
+//           resource: ['https://i.imgur.com/vdKC9kr.jpg']
+//         }, 
+//         {
+//           date: 'date 2',
+//           text: 'news 2 lalala',
+//           resource: ['https://i.imgur.com/ygZ5PGA.jpg']
+//         },
+//         {
+//           date: 'date 3',
+//           text: 'news 3 foo bar',
+//           resource: ['https://i.imgur.com/eqphLAA.jpg']
+//         },
+//         {
+//           date: 'date 1',
+//           text: 'news 1 aosndf',
+//           resource: ['https://i.imgur.com/vdKC9kr.jpg']
+//         }, 
+//         {
+//           date: 'date 2',
+//           text: 'news 2 lalala',
+//           resource: ['https://i.imgur.com/ygZ5PGA.jpg']
+//         },
+//         {
+//           date: 'date 3',
+//           text: 'news 3 foo bar',
+//           resource: ['https://i.imgur.com/eqphLAA.jpg']
+//         }
+//       ]);
+//     }, 500);
+//   });
+// }
